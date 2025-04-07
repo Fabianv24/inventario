34452074ad2a269935cbdf3f3939c48d192e9677
Fabian <fabianvargas199.fv@gmail.com>
2025-04-06 21:04:22 -0500

    first commit - web server

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+const main = () =>{
+
+    app.listen(app.get("port"));
+    console.log(`The company's web server web is running on port ${app.get("port")}`);
+}
+
+main();
